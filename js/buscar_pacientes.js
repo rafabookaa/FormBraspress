a01431cccc2481119205666c3ae2da61dbcd34a7
Rafael Ribeiro <59180005+rafabookaa@users.noreply.github.com>
2023-09-04 12:41:40 -0300

    erro ao não carregar arquivo

--- js/buscar_pacientes.js
+++ js/buscar_pacientes.js
@@ -1,23 +1,32 @@
+
+
 let botaoAdicionar = document.querySelector("#buscar-paciente");
-botaoAdicionar.addEventListener("click" , function () {
+botaoAdicionar.addEventListener("click" , (buscando) => {
   
   let xhr = new XMLHttpRequest(); //falando qual tipo de requisição quero
-  xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json"); // pegando os dados da API
+  xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/paciente11s.json"); // pegando os dados da API
 
-  xhr.addEventListener("load", function () { //carregando arquivo
-    let resposta = xhr.responseText;
-    let objPacientes = JSON.parse(resposta);
-        
+  xhr.addEventListener("load", (carregar) => { //carregando arquivo
+
+    if(xhr.status == 200) {let resposta = xhr.responseText;
+      let objPacientes = JSON.parse(resposta);
+      
+      objPacientes.forEach(function (objPacientes) {
+        adicionaPacienteNaTabela(objPacientes)
+        console.log("carregou")
+      });
+    } else {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+      let erroAjax = document.querySelector(".erro-ajax");
+      erroAjax.textContent = "Erro ao carregar tabela."
+    }
+
     
-   objPacientes.forEach(function (paciente) {
-    criaPaciente(paciente);
-    console.log(paciente);
-   });  
-  });
+    
+  })
   xhr.send();
-});
-
-
+} )
 
 // let botaoBuscar = addEventListener("click" , (buscar) => {
 //   alert("Botão clicado");
