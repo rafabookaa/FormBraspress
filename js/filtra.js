278c29581af5301f4db0d5037937a699f2932b04
Rafael Ribeiro <59180005+rafabookaa@users.noreply.github.com>
2023-09-04 12:43:59 -0300

    Update filtra.js

--- js/filtra.js
+++ js/filtra.js
@@ -1,31 +1,18 @@
-//criando um filtro para comparar o que foi digitado com o que tem cadastrado
-let campoFiltro = document.querySelector("#filtrar-tabela"); //pegando o input
-campoFiltro.addEventListener("input", function () {
-  //adicionando o evento ao input
-  let todosPacientes = document.querySelectorAll(".paciente"); //pegando os pacientes
 
-  if (this.value.length > 0) { // se digitar algo entra no if e faz o filtro
-    
-    for (let i = 0; i < todosPacientes.length; i++) {
-      let cadaPaciente = todosPacientes[i]; //verificando um por um
-      let tdNome = cadaPaciente.querySelector(".info-nome").textContent; //pegando o td e extraindo o conteudo
-      let expressao = new RegExp(this.value, "i" ) // criando a expressao regular para filtrar por letras 
-      
+//pegando todas as uls
+// let ulPacientes = document.querySelectorAll(".paciente")
 
-      //comparando o input com o nome..
-      if (!expressao.test(tdNome)) { //chamadno a expressao, testando e jogando o nome dentro. A expressao contem o this.value já e buscará letra por letra
-        //se diferente acrescenta a classe para sumir o td
-        cadaPaciente.classList.add("someNoFiltro");
-      } else {
-        //Se igual, mantem a lista como esta (não adiciona a classe de sumir)
-        cadaPaciente.classList.remove("someNoFiltro");
-      }
-    }
-  } else {
-    for (let i = 0; i < todosPacientes.length; i++) { //senão difgitar ele não ativa o filtro (não ativa a classe)
-      let cadaPaciente = todosPacientes[i];
-      cadaPaciente.classList.remove("someNoFiltro");
-    }
-  }
-});
+var tabela = document.querySelector("table")
+
+//event: evento
+//target: alvo (no caso o que foi clicado)
+//parentNode: pegando o pai do elemento clicado
+//remove(): vai remover o elemento clicado
+  tabela.addEventListener("dblclick", (event) => {
+    event.target.parentNode.classList.add("fadeOut") //pegando a classe para apagar lentamente
+
+    setTimeout ((remove) => {               // Definindo o tempo para remoção
+      event.target.parentNode.remove();
+    }, 500);    
+  }); 
 
